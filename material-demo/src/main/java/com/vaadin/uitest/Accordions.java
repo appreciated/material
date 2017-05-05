66d7a3bddbde4c0037e0af76242acc6a23b4f09d
appreciated <goebeljohannes@gmx.net>
2017-05-06 01:01:13 +0200

    Update Accordions.java

--- material-demo/src/main/java/com/vaadin/uitest/Accordions.java
+++ material-demo/src/main/java/com/vaadin/uitest/Accordions.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.uitest;
+package com.vaadin.uitest; // Changed Package Path
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
