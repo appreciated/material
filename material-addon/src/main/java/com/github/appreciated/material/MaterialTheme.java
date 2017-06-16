ebed121a816277ced00e12135f50e411deb029ea
appreciated <goebeljohannes@gmx.net>
2017-06-16 23:04:51 +0200

    0.7.2 Feature:  - Added possiblity to add v-datefields, v-textfield and v-filterselect with a custom color ("custom") Fixes:  - Style issues in v-reconnect-dialog  - Style issues in v-filterselect inside of a group

--- material-addon/src/main/java/com/github/appreciated/material/MaterialTheme.java
+++ material-addon/src/main/java/com/github/appreciated/material/MaterialTheme.java
@@ -13,6 +13,9 @@ public class MaterialTheme extends ValoTheme {
     public static final String TEXTFIELD_FLOATING = "floating";
     public static final String COMBOBOX_FLOATING = "floating";
     public static final String DATEFIELD_FLOATING = "floating";
+    public static final String TEXTFIELD_CONTRAST = "contrast";
+    public static final String COMBOBOX_CONTRAST = "contrast";
+    public static final String DATEFIELD_CONTRAST = "contrast";
     public static final String CARD_0 = "card-0";
     public static final String CARD_1 = "card-1";
     public static final String CARD_1_5 = "card-1-5";
